ca0d19c26ba620b26eb534362f8a8debeb12fddf
alvgrv <aaron.lovegrove@essentia-analytics.com>
2022-09-27 16:47:46 +0100

    s3 bucket set up as http only website

--- infra/main.tf
+++ infra/main.tf
@@ -60,7 +60,62 @@ resource "aws_s3_bucket" "site_bucket" {
 
 resource "aws_s3_bucket_acl" "site_bucket" {
   bucket = aws_s3_bucket.site_bucket.id
-  acl    = "private"
+  acl    = "public-read"
 }
 
 
+resource "aws_s3_bucket_website_configuration" "site_bucket" {
+  bucket = aws_s3_bucket.site_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "site_bucket" {
+  bucket = aws_s3_bucket.site_bucket.id
+  policy = data.aws_iam_policy_document.site_bucket.json
+}
+
+data "aws_iam_policy_document" "site_bucket" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.site_bucket.arn}/*"
+    ]
+  }
+}
+#
+#resource "aws_acm_certificate" "site_bucket" {
+#  provider                  = aws.acm_provider
+#  domain_name               = var.domain_name
+#  subject_alternative_names = ["*.${var.domain_name}"]
+#  validation_method         = "EMAIL"
+#  #validation_method = "DNS"
+#
+#  tags = var.common_tags
+#
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#}
+#
+## Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
+#resource "aws_acm_certificate_validation" "cert_validation" {
+#  provider        = aws.acm_provider
+#  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+#  #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+#}
